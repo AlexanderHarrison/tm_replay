0ca694deb8cb910bae9280d1bc757756ec66768a
Alex Harrison <alexander.harrison.r@gmail.com>
2025-06-16 18:37:56 -0600

    add run.sh

---
+++ utils/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+./replace_recsave "/home/alex/.config/SlippiOnline/GC/USA/Card A/$BAD_GCI" "$BAD_GCI" "$BAD_GCI.recsave"
+ ~/melee/Ishiiruka/Slippi_Online-x86_64.AppImage -b -e "/home/alex/melee/tm/TrainingMode-More/TM-CE.iso"
